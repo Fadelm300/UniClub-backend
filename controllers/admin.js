688966773c624871c3d9abe5fe142a7fb4c5444e
alihadi202 <202008189@stu.uob.edu.bh>
2024-09-29 06:14:16 +0300

    add admin and remove admin

---
+++ controllers/admin.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const User = require('../models/user');
+const Channel = require("../models/channel.js");
+const router = express.Router();
+const verifyToken = require('../middleware/verify-token');
+const user = require("../models/user");
+
+router.get('/users',verifyToken, async (req, res) => {
+    try {
+
+        const page = 1; // You can change this dynamically based on the request
+        const limit = 20;
+        const users = await User.find().skip((page - 1) * limit).limit(limit);
+      res.json(users);
+
+    } catch (error) {
+      if (res.statusCode === 404) {
+        res.status(404).json({ error: error.message });
+      } else {
+        res.status(500).json({ error: error.message });
+      }
+    }
+  });
+
+router.put('/toggle/:userid' ,  async (req , res) => {
+    const user = await User.findById(req.params.userid);
+    if (user.admin){
+        user.admin = false;
+        await user.save();
+    }else{
+        user.admin = true;
+        await user.save();
+    }
+    res.json(user);
+
+});
+
+  module.exports = router;
