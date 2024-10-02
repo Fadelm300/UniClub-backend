b787dabaea066c2564b8c48c6026228822fa83f9
Fadem300 <fadel.m200@gmail.com>
2024-10-02 19:38:18 +0300

    EventBackend

---
+++ models/Event.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    time: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    logo: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Event', eventSchema);
