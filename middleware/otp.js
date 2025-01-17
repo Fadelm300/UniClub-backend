a95602ebc443097ecdeb68ed6b59d8b297aa4655
alihadi202 <202008189@stu.uob.edu.bh>
2025-01-17 20:31:32 +0300

    reset password working

--- middleware/otp.js
+++ middleware/otp.js
@@ -14,7 +14,7 @@ sendSmtpEmail.htmlContent = `
 <html>
 <body style="font-family: Arial, sans-serif; background-color: #f4f4f9; color: #333; margin: 0; padding: 0;">
   <div style="max-width: 400px; margin: 50px auto; background: #ffffff; border-radius: 10px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1); padding: 20px; text-align: center;">
-    <h1 style="color: #4CAF50; font-size: 24px; margin-bottom: 10px;">One Time Passcode</h1>
+    <h1 style="color:rgb(56, 59, 245); font-size: 24px; margin-bottom: 10px;">One Time Passcode</h1>
     <h4 style="font-size: 16px; color: #555; margin-bottom: 20px;">Use this code and do not share it with anyone.</h4>
     <h1 style="font-size: 32px; color: #333; background: #f9f9f9; border: 1px dashed #ddd; padding: 10px 20px; border-radius: 5px; display: inline-block;">${otp}</h1>
   </div>
--- models/tempuser.js
+++ models/tempuser.js
@@ -4,23 +4,18 @@ const mongoose = require('mongoose');
 const tempuserSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true
   },
   hashedPassword: {
     type: String,
-    required: true
   },
   phone: {
     type: String,
-    required: true
   },
   email: {
     type: String,
-    required: true,
   },
   otp:{
     type: Number,
-    required: true
   },
   admin:{
     type: Boolean
